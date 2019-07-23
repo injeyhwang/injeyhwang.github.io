ca2973eaf06cae9cb35620384c75e0c049c4030f
Michael Hwang <michaelinjeyhwang@gmail.com>
2019-07-23 13:21:39 -0400

    Added CNAME and custom domain

---
+++ src/components/Card.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Card extends Component {
+
+  render() {
+    const { cardName, cardImage, annualFee, benefits } = this.props
+    return (
+      <div className='footer'>
+        
+      </div>
+    );
+  }
+}
+
+export default Card;
