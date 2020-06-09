ee4d0417ccabcca5a1ab511a60a3c118b65bf6ad
Michael Hwang <michaelinjeyhwang@gmail.com>
2020-06-09 14:44:24 -0400

    removed router

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
 import React from 'react';
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 
 import ThemeContainer from './components/ThemeContainer';
 import Header from './components/Header';
 import HomeView from './views/HomeView';
-import ResumeView from './views/ResumeView';
 import Footer from './components/Footer';
 
 export default function App() {
   return (
-    <Router>
-      <Switch>
-        <ThemeContainer>
-          <Header />
-          <Route path="/" component={HomeView} exact />
-          <Route path="/resume" component={ResumeView} />
-          <Footer />
-        </ThemeContainer>
-      </Switch>
-    </Router>
+    <ThemeContainer>
+      <Header />
+      <HomeView />
+      <Footer />
+    </ThemeContainer>
   );
 }
